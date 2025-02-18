c2c36225bb7cef281ec5bda881f1697e773125fa
Mark <mark.corray.off@gmail.com>
2025-02-18 21:16:29 +0300

    fix: small bug

--- task_tracker/migrations/20250216195552_add_profile_id_table_in_employees_table.sql
+++ task_tracker/migrations/20250216195552_add_profile_id_table_in_employees_table.sql
@@ -1,9 +1,9 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE employees ADD COLUMN profile_id TEXT NOT NULL DEFAULT '';
+ALTER TABLE employees ADD COLUMN profile TEXT NOT NULL DEFAULT '';
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-ALTER TABLE employees DROP COLUMN profile_id;
+ALTER TABLE employees DROP COLUMN profile;
 -- +goose StatementEnd
